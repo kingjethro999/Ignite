ad4505fbf66b0510164f78f332ad42c05c84a155
King Jethro <jethrojerrybj@gmail.com>
2025-08-06 12:08:53 +0100

    Update version to 0.2.1 and remove extension files

--- extension/extension.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-  // No custom activation logic needed for syntax highlighting and icon
-}
-
-export function deactivate() {} 
